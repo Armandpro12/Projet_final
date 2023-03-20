b1937686e8de85cb5c6569604fdad2d7213d217d
Armandpro12 <chaplainduretarmandpro@gmail.com>
2023-03-20 14:53:25 +0100

    add login activity and clear login buttons from activity main

--- app/src/main/java/fr/bam/projetfinal/MainActivity.java
+++ app/src/main/java/fr/bam/projetfinal/MainActivity.java
@@ -10,59 +10,10 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mUsernameInput;
-    private EditText mPasswordInput;
-    private Button mLoginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mUsernameInput = findViewById(R.id.main_username_edittext);
-        mPasswordInput = findViewById(R.id.main_password_edittext);
-        mLoginButton = findViewById(R.id.main_login_button);
-        mLoginButton.setEnabled(false);
-        mUsernameInput.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                checkFields();
-            }
-        });
-
-        mPasswordInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                checkFields();
-            }
-        });
-    }
-
-    private void checkFields() {
-        String username = mUsernameInput.getText().toString();
-        String password = mPasswordInput.getText().toString();
-        if (username.isEmpty() || password.isEmpty()) {
-            mLoginButton.setEnabled(false);
-        } else {
-            mLoginButton.setEnabled(true);
-        }
     }
 }
