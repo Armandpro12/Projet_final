acda207276a34c24e918db91205e878a2e6e0c99
Armandpro12 <chaplainduretarmandpro@gmail.com>
2023-03-20 12:29:47 +0100

    first login view with username/password input and login button

--- app/src/main/java/fr/bam/projetfinal/MainActivity.java
+++ app/src/main/java/fr/bam/projetfinal/MainActivity.java
@@ -3,12 +3,21 @@ package fr.bam.projetfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText mUsernameInput;
+    private EditText mPasswordInput;
+    private Button mLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUsernameInput = findViewById(R.id.main_username_edittext);
+        mPasswordInput = findViewById(R.id.main_password_edittext);
+        mLoginButton = findViewById(R.id.main_login_button);
+        mLoginButton.setEnabled(false);
     }
 }
