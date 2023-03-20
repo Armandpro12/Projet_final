89cb16d9075c47a4fe8e3fadd58b5796a2f4c1bf
Armandpro12 <chaplainduretarmandpro@gmail.com>
2023-03-20 15:18:37 +0100

    first add DB (to modify) and User, Doctor and Patient

---
+++ app/src/main/java/fr/bam/projetfinal/model/Doctor.java
@@ -0,0 +1,29 @@
+package fr.bam.projetfinal.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doctor extends User{
+    List<Patient> mPatients;
+    public Doctor(String firstName, String lastName, String address, String email, byte[] photo) {
+        super(firstName, lastName, address, email, photo);
+        this.mPatients = new ArrayList<>();
+    }
+
+    public Doctor() {
+        super();
+        this.mPatients = new ArrayList<>();
+    }
+
+    public void addPatient(Patient patient){
+        mPatients.add(patient);
+    }
+
+    public List<Patient> getPatients() {
+        return mPatients;
+    }
+
+    public void setPatients(List<Patient> patients) {
+        mPatients = patients;
+    }
+}
---
+++ app/src/main/java/fr/bam/projetfinal/model/Patient.java
@@ -0,0 +1,21 @@
+package fr.bam.projetfinal.model;
+
+public class Patient extends User{
+    Doctor mDoctor;
+    public Patient(String firstName, String lastName, String address, String email, byte[] photo, Doctor doctor) {
+        super(firstName, lastName, address, email, photo);
+        mDoctor = doctor;
+    }
+
+    public Patient() {
+        super();
+    }
+
+    public Doctor getDoctor() {
+        return mDoctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        mDoctor = doctor;
+    }
+}
