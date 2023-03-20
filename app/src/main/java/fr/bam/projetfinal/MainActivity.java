78689531c4bf255edeb5311989721a4cefe661e7
Armandpro12 <chaplainduretarmandpro@gmail.com>
2023-03-20 15:22:09 +0100

    MainActivity start LoginActivity by default

--- app/src/main/java/fr/bam/projetfinal/MainActivity.java
+++ app/src/main/java/fr/bam/projetfinal/MainActivity.java
@@ -2,6 +2,7 @@ package fr.bam.projetfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -15,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
