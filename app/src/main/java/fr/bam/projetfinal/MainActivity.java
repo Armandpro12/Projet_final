a5ad9d616c34d13d60d9908d09b2447e348500ec
Armandpro12 <chaplainduretarmandpro@gmail.com>
2023-03-20 14:40:09 +0100

    basic login activity with field check

--- app/src/main/java/fr/bam/projetfinal/MainActivity.java
+++ app/src/main/java/fr/bam/projetfinal/MainActivity.java
@@ -3,6 +3,8 @@ package fr.bam.projetfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,5 +21,48 @@ public class MainActivity extends AppCompatActivity {
         mPasswordInput = findViewById(R.id.main_password_edittext);
         mLoginButton = findViewById(R.id.main_login_button);
         mLoginButton.setEnabled(false);
+        mUsernameInput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                checkFields();
+            }
+        });
+
+        mPasswordInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                checkFields();
+            }
+        });
+    }
+
+    private void checkFields() {
+        String username = mUsernameInput.getText().toString();
+        String password = mPasswordInput.getText().toString();
+        if (username.isEmpty() || password.isEmpty()) {
+            mLoginButton.setEnabled(false);
+        } else {
+            mLoginButton.setEnabled(true);
+        }
     }
 }
