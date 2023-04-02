283031c7b60f174533c9d0eeb7b2d5cbb0d5aaec
Etoile2003 <95474735+Etoile2003@users.noreply.github.com>
2023-04-02 15:40:41 +0200

    creating doctor view and activity activity_doctor_view.xml is now scrollable

---
+++ app/src/main/java/fr/bam/projetfinal/DoctorActivity.java
@@ -0,0 +1,15 @@
+package fr.bam.projetfinal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DoctorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_view);
+
+
+    }
+}
